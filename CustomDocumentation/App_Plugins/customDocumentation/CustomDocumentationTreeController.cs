8340e52f980206fbb9bf29466a8ff64b019ac118
Scott Meysman <scottmeysman68@gmail.com>
2019-06-18 22:05:26 +0200

    Added basic tree controller

--- CustomDocumentation/App_Plugins/customDocumentation/CustomDocumentationTreeController.cs
+++ CustomDocumentation/App_Plugins/customDocumentation/CustomDocumentationTreeController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Net.Http.Formatting;
-using System.Web;
-using System.Web.Http.ModelBinding;
 using Umbraco.Web.Models.Trees;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.Trees;
-using Umbraco.Web.WebApi.Filters;
 
 namespace CustomDocumentation.App_Plugins.customDocumentation
 {
@@ -15,14 +11,47 @@ namespace CustomDocumentation.App_Plugins.customDocumentation
     [PluginController("customDocumentation")]
     public class CustomDocumentationTreeController : TreeController
     {
-        protected override TreeNodeCollection GetTreeNodes(string id, [ModelBinder(typeof(HttpQueryStringModelBinder))] FormDataCollection queryStrings)
+        protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
         {
-            return new TreeNodeCollection();
+            ReadFolder();
+            return CreateTreeNodeCollection(id, queryStrings);
         }
 
-        protected override MenuItemCollection GetMenuForNode(string id, [ModelBinder(typeof(HttpQueryStringModelBinder))] FormDataCollection queryStrings)
+        protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
         {
             throw new NotImplementedException();
+        }
+
+        private TreeNodeCollection CreateTreeNodeCollection(string id, FormDataCollection queryStrings)
+        {
+            var nodes = new TreeNodeCollection();
+            if (id == "-1")
+            {
+                return TopLevel(id, queryStrings);
+            }
+            return nodes;
+        }
+
+        private TreeNodeCollection TopLevel(string id, FormDataCollection queryStrings)
+        {
+            var topNodes = new TreeNodeCollection();
+            topNodes.Add(CreateTreeNode("Top", id, queryStrings, "Documentation", "icon-folder", true));
+            return topNodes;
+        }
+
+        //private TreeNodeCollection GetChildren(string id, FormDataCollection queryStrings)
+        //{
+        //    var 
+        //    return new TreeNodeCollection();
+        //}
+
+        private void ReadFolder()
+        {
+            var path = "~/Documentation";
+            var directoryName = Path.GetFileName(path);
+            var currentDirectoryPath = Directory.GetCurrentDirectory();
+            var res = AppDomain.CurrentDomain.BaseDirectory;
+            var directories = Directory.GetDirectories(currentDirectoryPath+ @"\Documentation");
         }
     }
 }
