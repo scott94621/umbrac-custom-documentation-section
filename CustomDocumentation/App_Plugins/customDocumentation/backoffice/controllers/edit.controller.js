7f6848743e7a30cd393f4641d03a690e1d13fd31
Scott Meysman <scottmeysman68@gmail.com>
2019-06-26 21:42:50 +0200

    Added markdown library, controller for converting, angular controller for frontend

---
+++ CustomDocumentation/App_Plugins/customDocumentation/backoffice/controllers/edit.controller.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+angular.module('umbraco').controller('cDoc.DocumentationController', ['$scope', 'notificationsService', function ($scope, notificationsService) {
+
+    function getHtmlCode(url) {
+        let successResult = "";
+        $.ajax({
+            url: url,
+            dataType: "html",
+            async: false,
+            success: function (data) {
+                successResult = data;
+            },
+            error: function (jqXHR, textStatus) {
+                notificationsService.error("Something went wrong while getting the html code. " + textStatus);
+            }
+        });
+        return successResult;
+    }
+
+    function setHtmlCode(htmlCode) {
+        $("#markdownDiv").html(htmlCode);
+    }
+
+    function init() {
+        var htmlCode = getHtmlCode("/umbraco/backoffice/api/CustomDocumentation/GetHtmlForRoute");
+        setHtmlCode(htmlCode);
+
+    }
+
+    init();
+}]);
---
+++ CustomDocumentation/Controllers/CustomDocumentationController.cs
@@ -0,0 +1,22 @@
+﻿using Markdig;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace Controllers
+{
+    public class CustomDocumentationController: UmbracoAuthorizedApiController
+    {
+        public string GetHtmlForRoute()
+        {
+            var result = Markdown.ToHtml("This is a text with some *emphasis*");
+            return result;
+        }
+
+    }
+}
