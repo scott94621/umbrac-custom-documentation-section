1495e79f6dc9ac057a6f7045eb5d0f2dda9d804b
Scott Meysman <scottmeysman68@gmail.com>
2019-08-19 19:52:29 +0200

    Fixed the package so it compiles and unpackages correctly

--- CustomDocumentation/App_Plugins/customDocumentation/ExtensionHelper.cs
+++ CustomDocumentation/App_Plugins/customDocumentation/ExtensionHelper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CustomDocumentation.App_Plugins.customDocumentation
+﻿namespace CustomDocumentation.App_Plugins.customDocumentation
 {
     public static class ExtensionHelper
     {
